ce0be1a60a74727d52d0713e22456da979560b9c
Vinicius Sa <viniciusjavs@gmail.com>
2020-11-08 00:52:08 -0300

    Improve: Hamming

--- hamming/hamming.cpp
+++ hamming/hamming.cpp
@@ -1,19 +1,14 @@
 #include "hamming.h"
+#include <numeric>
 
 namespace hamming {
 
 int compute(string const &first, string const &second) {
-  auto len = first.length();
-  if (len != second.length())
-    throw std::domain_error{"sequences differ in length"};
+  if (first.length() != second.length())
+    throw std::domain_error{"Sequences differ in length."};
 
-  int result{0};
-
-  for (std::size_t i{0}; i <= len; ++i)
-    if (first[i] != second[i])
-      ++result;
-
-  return result;
+  return std::inner_product(begin(first), end(first), begin(second), 0,
+                            std::plus<>{}, std::not_equal_to<>{});
 }
 
 } // namespace hamming
