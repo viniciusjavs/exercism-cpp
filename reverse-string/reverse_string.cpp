f70e616b897eb784663c4936cdd304902a6fb77a
Vinicius Sa <viniciusjavs@gmail.com>
2020-11-08 14:18:22 -0300

    Solution: Reverse String

--- reverse-string/reverse_string.cpp
+++ reverse-string/reverse_string.cpp
@@ -1,5 +1,11 @@
 #include "reverse_string.h"
+#include <algorithm>
 
 namespace reverse_string {
 
-}  // namespace reverse_string
+string reverse_string(string str) {
+  std::reverse(begin(str), end(str));
+  return str;
+}
+
+} // namespace reverse_string
--- reverse-string/reverse_string.h
+++ reverse-string/reverse_string.h
@@ -1,8 +1,14 @@
 #if !defined(REVERSE_STRING_H)
 #define REVERSE_STRING_H
 
+#include <string>
+
 namespace reverse_string {
 
-}  // namespace reverse_string
+using std::string;
+
+string reverse_string(string str);
+
+} // namespace reverse_string
 
 #endif // REVERSE_STRING_H
