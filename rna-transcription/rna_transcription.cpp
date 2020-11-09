340d7857d1c49e92d3787e1e39d8793a18672455
Vinicius Sa <viniciusjavs@gmail.com>
2020-11-09 16:32:11 -0300

    Solution: RNA Transcription

--- rna-transcription/rna_transcription.cpp
+++ rna-transcription/rna_transcription.cpp
@@ -1,5 +1,19 @@
 #include "rna_transcription.h"
+#include <algorithm>
+#include <unordered_map>
 
 namespace rna_transcription {
 
-}  // namespace rna_transcription
+std::unordered_map<char, char> const dna_to_rna{
+    {'G', 'C'}, {'C', 'G'}, {'T', 'A'}, {'A', 'U'}};
+
+char to_rna(char dna) { return dna_to_rna.at(dna); }
+
+string to_rna(string dna) {
+  string rna;
+  std::transform(cbegin(dna), cend(dna), std::back_inserter(rna),
+                 static_cast<char (*)(char)>(to_rna));
+  return rna;
+}
+
+} // namespace rna_transcription
--- rna-transcription/rna_transcription.h
+++ rna-transcription/rna_transcription.h
@@ -1,8 +1,15 @@
 #if !defined(RNA_TRANSCRIPTION_H)
 #define RNA_TRANSCRIPTION_H
 
+#include <string>
+
 namespace rna_transcription {
 
-}  // namespace rna_transcription
+using std::string;
+
+char to_rna(char dna);
+string to_rna(string dna);
+
+} // namespace rna_transcription
 
 #endif // RNA_TRANSCRIPTION_H
