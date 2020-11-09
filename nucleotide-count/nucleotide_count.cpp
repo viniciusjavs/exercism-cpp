68e2364af712bc0a64708cfdb67f952fbf2eccd4
Vinicius Sa <viniciusjavs@gmail.com>
2020-11-09 01:38:19 -0300

    Solution: Nucleotide Count

--- nucleotide-count/nucleotide_count.cpp
+++ nucleotide-count/nucleotide_count.cpp
@@ -1,5 +1,38 @@
 #include "nucleotide_count.h"
 
+#include <algorithm>
+#include <stdexcept>
+
 namespace nucleotide_count {
 
-}  // namespace nucleotide_count
+using std::cbegin;
+using std::cend;
+
+bool counter::valid_key(char key) const {
+  char const keys[] = {'A', 'C', 'G', 'T'};
+  return std::any_of(cbegin(keys), cend(keys),
+                     [key](auto k) { return key == k; });
+}
+
+void counter::validate_key(char key) const {
+  if (!valid_key(key))
+    throw std::invalid_argument{"Invalid key."};
+}
+
+counter::counter(string dna) : dna_{dna} {
+  std::for_each(cbegin(dna_), cend(dna_), [this](auto key) {
+    validate_key(key);
+    ++nucleotides_[key];
+  });
+}
+
+std::map<char, int> const &counter::nucleotide_counts() const {
+  return nucleotides_;
+}
+
+int counter::count(char key) const {
+  validate_key(key);
+  return nucleotides_.at(key);
+}
+
+} // namespace nucleotide_count
--- nucleotide-count/nucleotide_count.h
+++ nucleotide-count/nucleotide_count.h
@@ -1,8 +1,26 @@
 #if !defined(NUCLEOTIDE_COUNT_H)
 #define NUCLEOTIDE_COUNT_H
 
+#include <map>
+#include <string>
+#include <unordered_set>
+
 namespace nucleotide_count {
 
-}  // namespace nucleotide_count
+using std::string;
+
+class counter {
+  string dna_;
+  std::map<char, int> nucleotides_{{'A', 0}, {'C', 0}, {'G', 0}, {'T', 0}};
+  bool valid_key(char key) const;
+  void validate_key(char key) const;
+
+public:
+  counter(string dna);
+  std::map<char, int> const &nucleotide_counts() const;
+  int count(char key) const;
+};
+
+} // namespace nucleotide_count
 
 #endif // NUCLEOTIDE_COUNT_H
