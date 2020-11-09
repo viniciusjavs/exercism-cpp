602fbfd152876d97bb15526bbfd5f19ae155a797
Vinicius Sa <viniciusjavs@gmail.com>
2020-11-09 11:40:30 -0300

    Solution: Grade School

--- grade-school/grade_school.cpp
+++ grade-school/grade_school.cpp
@@ -1,5 +1,14 @@
 #include "grade_school.h"
+#include <algorithm>
 
 namespace grade_school {
-
-}  // namespace grade_school
+roster_t const &school::roster() const { return roster_; }
+void school::add(string name, int grade) {
+  auto &students = roster_[grade];
+  students.push_back(name);
+  std::sort(begin(students), end(students));
+}
+vector<string> school::grade(int grade) const {
+  return roster_.count(grade) == 0 ? vector<string>{} : roster_.at(grade);
+}
+} // namespace grade_school
--- grade-school/grade_school.h
+++ grade-school/grade_school.h
@@ -1,8 +1,26 @@
 #if !defined(GRADE_SCHOOL_H)
 #define GRADE_SCHOOL_H
 
+#include <map>
+#include <string>
+#include <vector>
+
 namespace grade_school {
 
-}  // namespace grade_school
+using std::string;
+using std::vector;
+
+using roster_t = std::map<int, vector<string>>;
+
+class school {
+  roster_t roster_;
+
+public:
+  roster_t const &roster() const;
+  void add(string name, int grade);
+  vector<string> grade(int grade) const;
+};
+
+} // namespace grade_school
 
 #endif // GRADE_SCHOOL_H
