509cc50237237618e1acbf9af708b16c6f39eb99
Vinicius Sa <viniciusjavs@gmail.com>
2020-11-10 22:17:26 -0300

    Solution: Difference Of Squares

--- difference-of-squares/difference_of_squares.cpp
+++ difference-of-squares/difference_of_squares.cpp
@@ -1,5 +1,23 @@
 #include "difference_of_squares.h"
 
+#include <cmath>
+#include <numeric>
+#include <vector>
+
 namespace difference_of_squares {
 
-}  // namespace difference_of_squares
+int square_of_sum(int n) {
+  std::vector<int> ints(n);
+  std::iota(begin(ints), end(ints), 1);
+  return std::pow(std::accumulate(cbegin(ints), cend(ints), 0), 2);
+}
+
+int sum_of_squares(int n) {
+  std::vector<int> ints(n);
+  std::iota(begin(ints), end(ints), 1);
+  return std::inner_product(cbegin(ints), cend(ints), cbegin(ints), 0);
+}
+
+int difference(int n) { return square_of_sum(n) - sum_of_squares(n); }
+
+} // namespace difference_of_squares
--- difference-of-squares/difference_of_squares_test.cpp
+++ difference-of-squares/difference_of_squares_test.cpp
@@ -8,7 +8,6 @@ TEST_CASE("up_to_5")
     REQUIRE(170 == difference_of_squares::difference(5));
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 TEST_CASE("up_to_10")
 {
     REQUIRE(3025 == difference_of_squares::square_of_sum(10));
@@ -22,4 +21,3 @@ TEST_CASE("up_to_100")
     REQUIRE(338350 == difference_of_squares::sum_of_squares(100));
     REQUIRE(25164150 == difference_of_squares::difference(100));
 }
-#endif
