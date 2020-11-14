4d48f29bcc28e9ce0f0b5e4063e867c24fa96074
Vinicius Sa <viniciusjavs@gmail.com>
2020-11-14 17:13:39 -0300

    Solution: Queen Attack

--- queen-attack/queen_attack.cpp
+++ queen-attack/queen_attack.cpp
@@ -2,4 +2,36 @@
 
 namespace queen_attack {
 
-}  // namespace queen_attack
+chess_board::chess_board(pair_t white, pair_t black)
+    : white_{white}, black_{black} {
+  if (white == black)
+    throw std::domain_error("Queen positions must be distinct");
+}
+
+chess_board::operator string() const {
+  string board;
+  board.reserve((size * 2) * size);
+  for (int row : iota(0, size)) {
+    for (int column : iota(0, size))
+      if (auto pair = std::make_pair(row, column); pair == white_)
+        board.append("W ");
+      else if (pair == black_)
+        board.append("B ");
+      else
+        board.append("_ ");
+    board.pop_back();
+    board.push_back('\n');
+  }
+  return board;
+}
+
+bool chess_board::can_attack() const {
+  if ((white_.first == black_.first) || (white_.second == black_.second))
+    return true;
+  if (std::abs(white_.second - black_.second) ==
+      std::abs(white_.first - black_.first))
+    return true;
+  return false;
+}
+
+} // namespace queen_attack
--- queen-attack/queen_attack.h
+++ queen-attack/queen_attack.h
@@ -1,8 +1,33 @@
 #if !defined(QUEEN_ATTACK_H)
 #define QUEEN_ATTACK_H
 
+#include <cmath>
+#include <ranges>
+#include <stdexcept>
+#include <utility>
+
 namespace queen_attack {
 
-}  // namespace queen_attack
+using pair_t = std::pair<int, int>;
+using std::string;
+using namespace std::views;
+
+class chess_board {
+  static constexpr auto size = 8;
+  pair_t white_;
+  pair_t black_;
+
+public:
+  chess_board(pair_t white = {0, 3}, pair_t black = {7, 3});
+
+  pair_t white() const { return white_; }
+  pair_t black() const { return black_; }
+
+  explicit operator string() const;
+
+  bool can_attack() const;
+};
+
+} // namespace queen_attack
 
 #endif // QUEEN_ATTACK_H
