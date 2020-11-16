a9757db146461e2ef48e3f38e12dfb0c4f0d819a
Vinicius Sa <viniciusjavs@gmail.com>
2020-11-16 00:59:39 -0300

    Solution: Sum Of Multiples

--- sum-of-multiples/sum_of_multiples.cpp
+++ sum-of-multiples/sum_of_multiples.cpp
@@ -1,5 +1,16 @@
 #include "sum_of_multiples.h"
 
+#include <numeric>
+#include <set>
+
 namespace sum_of_multiples {
 
-}  // namespace sum_of_multiples
+int to(std::initializer_list<int> numbers, int limit) {
+  std::set<int> multiples;
+  for (int number : numbers)
+    for (int n{number}; n < limit; n += number)
+      multiples.insert(n);
+  return std::reduce(cbegin(multiples), cend(multiples));
+}
+
+} // namespace sum_of_multiples
--- sum-of-multiples/sum_of_multiples.h
+++ sum-of-multiples/sum_of_multiples.h
@@ -1,8 +1,12 @@
 #if !defined(SUM_OF_MULTIPLES_H)
 #define SUM_OF_MULTIPLES_H
 
+#include <initializer_list>
+
 namespace sum_of_multiples {
 
-}  // namespace sum_of_multiples
+int to(std::initializer_list<int> numbers, int limit);
+
+} // namespace sum_of_multiples
 
 #endif // SUM_OF_MULTIPLES_H
