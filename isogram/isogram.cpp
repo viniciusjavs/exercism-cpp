89eebb7e573064f749716f46ac91d481f5aa1cb8
Vinicius Sa <viniciusjavs@gmail.com>
2020-11-23 00:48:40 -0300

    Solution: Isogram

--- isogram/isogram.cpp
+++ isogram/isogram.cpp
@@ -1,5 +1,16 @@
 #include "isogram.h"
 
+#include <algorithm>
+#include <cctype>
+
 namespace isogram {
 
-}  // namespace isogram
+bool is_isogram(string str) {
+  std::erase_if(str, [](unsigned char c) { return !std::isalpha(c); });
+  std::transform(cbegin(str), cend(str), begin(str),
+                 [](unsigned char c) { return std::tolower(c); });
+  std::sort(begin(str), end(str));
+  return std::adjacent_find(cbegin(str), cend(str)) == cend(str);
+}
+
+} // namespace isogram
--- isogram/isogram.h
+++ isogram/isogram.h
@@ -1,8 +1,14 @@
 #if !defined(ISOGRAM_H)
 #define ISOGRAM_H
 
+#include <string>
+
 namespace isogram {
 
-}  // namespace isogram
+using std::string;
+
+bool is_isogram(string str);
+
+} // namespace isogram
 
 #endif // ISOGRAM_H
